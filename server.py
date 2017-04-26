2cb83d4742eb9f2e6defbd999731c6464f1c4a97
Kyle Ryan <kylrya@gmail.com>
2017-04-25 21:48:48 -0400

    Latest

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask import render_template
 from flask import request
 #app = Flask(static_folder = '/Users/vishnuchittari/gesturelink')
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
@@ -25,11 +24,34 @@ def demo():
 def survey():
 	return render_template("survey.html")
 
-@app.route('/trydemo', methods=["GET"])
-def trydemo():
+@app.route('/lights-demo', methods=["GET"])
+def lightsDemo():
     type = request.args.get('type')
-    return render_template("trydemo.html")
+    return render_template("lights-demo.html")
 
+@app.route('/music-player-demo', methods=["GET"])
+def musicPlayerDemo():
+    type = request.args.get('type')
+    return render_template("music-player-demo.html")
+
+@app.route('/lock-doors-demo', methods=["GET"])
+def lockDoorsDemo():
+    type = request.args.get('type')
+    return render_template("lock-doors-demo.html")
+
+@app.route('/testing', methods=["GET"])
+def testing():
+    type = request.args.get('type')
+    return render_template("example_simple_exportwav.html")
+
+@app.route('/sound-example', methods=["GET"])
+def soundExample():
+    type = request.args.get('type')
+    return render_template("sound-example.html")
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 if __name__ == "__main__":
     app.run()
